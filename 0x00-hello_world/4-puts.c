5914df08ed4a522c3a24e992056d57defd86fe56
zulaykha <suliat7@gmail.com>
2023-06-15 17:59:36 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - I dont really understand anything put with function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
