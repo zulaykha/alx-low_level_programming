ce198c80a2c08139a1903760bd4385d8b6b14ffc
zulaykha <suliat7@gmail.com>
2023-06-15 18:12:18 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program i dont understand using the printf function
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
